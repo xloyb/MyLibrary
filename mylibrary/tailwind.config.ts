a17b41c5c7074efdf5f3e4129a322d1f0b47f336
mydevify <boussaadilouay9@gmail.com>
2024-08-18 20:17:10 +0100

    added DaisyUI

--- mylibrary/tailwind.config.ts
+++ mylibrary/tailwind.config.ts
@@ -1,3 +1,4 @@
+import daisyui from "daisyui";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -15,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
 export default config;

fff89fafb18a687132c79761a428d2f069c79375
mydevify <boussaadilouay9@gmail.com>
2024-08-20 19:22:17 +0100

    created BooksList comp

---
+++ mylibrary/src/components/Home/BooksList.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const BooksList = () => {
+  return (
+    <>
+     
+    <div className="bg-base-100 card mx-6 mt-5 md:pt-4 px-6">
+   <div className="text-xl font-semibold inline-block">Ebooks</div>
+   <span className='text-gray-500 text-xs'>Test Description.</span>
+   <div className="divider mt-2"></div>
+
+
+
+
+ </div>
+   </>
+  )
+}
+
+export default BooksList

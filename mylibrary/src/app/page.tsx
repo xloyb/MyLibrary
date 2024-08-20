9dc6c1d8416f354707022b18b4b88fa527a0b51b
mydevify <boussaadilouay9@gmail.com>
2024-08-20 20:59:06 +0100

    installed clerk for nextjs

--- mylibrary/src/app/page.tsx
+++ mylibrary/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
 
-      <MyHome/>
+      {/* <MyHome/> */}
       <Books/>
 
     </div>
--- mylibrary/src/components/Home.tsx
+++ mylibrary/src/components/Home.tsx
@@ -7,11 +7,9 @@ const MyHome = () => {
      
      <div className="bg-base-100 card mx-6 mt-5 md:pt-4 px-6">
     <div className="text-xl font-semibold inline-block">Authorized Countries</div>
-    <div className='text-gray-500 text-xs'>Some countries have restrictions on cryptocurrency transactions. From here, you can disable crypto payments for a service and keep only the other payment methods available.
-      
-    </div>
-
+    <div className='text-gray-500 text-xs'>Some countries have restrictions on cryptocurrency transactions. From here, you can disable crypto payments for a service and keep only the other payment methods available.</div>
     <div className="divider mt-2"></div>
+    
   </div>
     </>
 

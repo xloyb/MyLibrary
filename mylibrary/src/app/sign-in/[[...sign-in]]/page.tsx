5c0f107b738c340807c730bbc893e25981d154ba
mydevify <boussaadilouay9@gmail.com>
2024-08-20 22:08:29 +0100

    centred login page

--- mylibrary/src/app/sign-in/[[...sign-in]]/page.tsx
+++ mylibrary/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,11 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-md w-full">
+        <SignIn />
+      </div>
+    </div>
+  );
 }

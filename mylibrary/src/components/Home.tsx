566d2bc43771a0084200cb4c11695a7a1729b1e4
mydevify <boussaadilouay9@gmail.com>
2024-08-20 17:42:24 +0100

    created homepage

---
+++ mylibrary/src/components/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MyHome = () => {
+  return (
+    <div className="bg-base-100 card mx-6 mt-5 md:pt-4 px-6">
+    <div className="text-xl font-semibold inline-block">Authorized Countries</div>
+    <div className='stat-desc text-xs'>Some countries have restrictions on cryptocurrency transactions. From here, you can disable crypto payments for a service and keep only the other payment methods available.</div>
+    <div className="divider mt-2"></div>
+
+  
+  </div>
+  )
+}
+
+export default MyHome

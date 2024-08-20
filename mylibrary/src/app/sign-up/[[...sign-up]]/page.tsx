806f4fc72cec8b2a9250e98947a59f3bd1ead24e
mydevify <boussaadilouay9@gmail.com>
2024-08-20 22:08:47 +0100

    centred registration page

--- mylibrary/src/app/sign-up/[[...sign-up]]/page.tsx
+++ mylibrary/src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-md w-full">
+        <SignUp />
+      </div>
+    </div>
+  );
 }

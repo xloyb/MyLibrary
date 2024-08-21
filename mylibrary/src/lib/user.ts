0aad5ec5f2d3dad687d5c5c241330f77eefc8c49
mydevify <boussaadilouay9@gmail.com>
2024-08-21 13:23:41 +0100

    created getUserByEmail

--- mylibrary/src/lib/user.ts
+++ mylibrary/src/lib/user.ts
@@ -11,3 +11,16 @@ export async function getUserById(id: number) {
       throw new Error(`Failed to retrieve user with ID ${id}: ${err.message}`);
     }
   }
+
+
+  export async function getUserByEmail(email: string) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
+      return user;
+    } catch (error) {
+      const err = error as Error;
+      throw new Error(`Failed to retrieve user with email ${email}: ${err.message}`);
+    }
+  }

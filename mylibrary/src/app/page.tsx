5b6b0390db5735a74fbc587078dd224ffa06f416
mydevify <boussaadilouay9@gmail.com>
2024-08-23 21:48:45 +0100

    created categries comp

--- mylibrary/src/app/page.tsx
+++ mylibrary/src/app/page.tsx
@@ -1,14 +1,14 @@
 import Books from "@/components/Home/BooksList";
 import Layout from "./layout";
 import MyHome from "@/components/Home";
+import Categories from "@/components/categories/Categories";
 
 export default function Home() {
   return (
     <div>
 
       {/* <MyHome/> */}
-      <Books/>
-
+       <Categories/>
     </div>
   );
 }

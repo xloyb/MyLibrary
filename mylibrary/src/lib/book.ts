15ce2bc1cd6188114da6c9d7fb165314a42c8353
mydevify <boussaadilouay9@gmail.com>
2024-08-23 23:15:37 +0100

    created getBooksByCategoryServername

--- mylibrary/src/lib/book.ts
+++ mylibrary/src/lib/book.ts
@@ -14,3 +14,22 @@ export const getAllBooksWithCategory = async () => {
     throw error;
   }
 };
+
+
+export async function getBooksByCategoryServername(servername: string) {
+  try {
+    const category = await prisma.category.findUnique({
+      where: { servername },
+    });
+    if (!category) {
+      throw new Error('Category not found');
+    }
+    const books = await prisma.book.findMany({
+      where: { categoryId: category.id }, 
+    });
+    return books;
+  } catch (error) {
+    console.error('Failed to fetch books by category servername:', error);
+    throw new Error(`Failed to fetch books`);
+  }
+}

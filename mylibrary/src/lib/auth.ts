0b8597b5e842e38ba88a1d97f8eb70c73b59e829
mydevify <boussaadilouay9@gmail.com>
2024-08-23 23:53:27 +0100

    created isTeam

--- mylibrary/src/lib/auth.ts
+++ mylibrary/src/lib/auth.ts
@@ -9,3 +9,11 @@ export async function isMod(clerkUserId: string): Promise<boolean> {
   const user = await getUserByClerkUserId(clerkUserId);
   return user?.isMod ?? false;
 }
+
+//to check if that mother fucker is Mod or Admin
+export async function isTeam(clerkUserId: string): Promise<boolean> {
+  const user = await getUserByClerkUserId(clerkUserId);
+  const isMod = user?.isMod ?? false;
+  const isAdmin = user?.isAdmin ?? false;
+  return isMod || isAdmin;
+}

b1e1b604026f219f68844ed07fce6383b53fde3d
mydevify <boussaadilouay9@gmail.com>
2024-09-03 14:52:58 +0100

    added authorization to the Books Page

--- mylibrary/src/app/admincp/books/page.tsx
+++ mylibrary/src/app/admincp/books/page.tsx
@@ -1,12 +1,49 @@
+"use client";
+import DeniedAccessMotherFucker from "@/components/CustomPages/403";
+import Loading from "@/components/Loading";
+import { useUser } from "@clerk/nextjs";
+import React, { useEffect, useState } from "react";
 import ManageBooks from '@/components/admincp/books/ManageBooks'
-import React from 'react'
 
 const BooksPage = () => {
+  const { user } = useUser();
+  const [loading, setLoading] = useState<boolean>(true);
+  // const [isAdminMember, setisAdminMember] = useState<boolean>(false);
+  const [isTeamMember, setisTeamMember] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchUserRoles = async () => {
+      if (user?.id) {
+        try {
+          const response = await fetch(`/api/userRoles?clerkUserId=${user.id}`);
+          if (response.ok) {
+            const fetchedUser = await response.json();
+            setisTeamMember(fetchedUser?.isAdmin || false);
+            setLoading(false);
+          } else {
+            console.error('Failed to fetch user roles');
+          }
+        } catch (error) {
+          console.error('Error fetching user roles:', error);
+        }
+      }
+    };
+    
+    fetchUserRoles();
+  }, [user]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <div>
-        <ManageBooks/>
+      {isTeamMember ? <ManageBooks/> : <DeniedAccessMotherFucker />}
     </div>
-  )
-}
+  );
+};
+
 
 export default BooksPage
+
+

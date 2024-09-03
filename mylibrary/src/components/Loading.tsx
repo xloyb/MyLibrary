d4a26a61cdb5d23d45f4822f3f979c2f21dd2f5e
mydevify <boussaadilouay9@gmail.com>
2024-09-03 14:34:50 +0100

    created loading comp

---
+++ mylibrary/src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <span className="loading loading-ring loading-lg"></span>
+        </div>
+    )
+}
+
+export default Loading

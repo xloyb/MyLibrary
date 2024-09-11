db48f7565c06c801db5af9721587f64108c5b4de
mydevify <boussaadilouay9@gmail.com>
2024-09-11 01:21:12 +0100

    created about page

---
+++ mylibrary/src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Mydevify from '@/components/about/mydevify'
+import AboutMyLibrary from '@/components/about/mylibrary'
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+       <>
+       <Mydevify/>
+       <AboutMyLibrary/>
+       </>
+    )
+}
+
+export default AboutPage

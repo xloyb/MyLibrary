1ab1a7d643bb8e0212fa3616767296329235acb0
mydevify <boussaadilouay9@gmail.com>
2024-09-11 01:21:01 +0100

    updated layout, footer now sticky

--- mylibrary/src/app/layout.tsx
+++ mylibrary/src/app/layout.tsx
@@ -52,6 +52,63 @@
 // export default Layout;
 
 
+// import { ReactNode } from 'react';
+// import Sidebar from '@/components/Sidebar';
+// import styles from '@/app/main.module.css';
+// import Navbar from '@/components/Navbar';
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import {
+//   ClerkProvider,
+//   SignInButton,
+//   SignedIn,
+//   SignedOut,
+//   UserButton
+// } from '@clerk/nextjs'
+// import Footer from '@/components/Footer';
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "MyLibrary",
+//   description: "Easily access and download a wide range of IT eBooks. Browse by category and get the resources you need with just a click—no limits, no hassle. Dive into your next great read today!",
+// };
+// interface LayoutProps {
+//   children: ReactNode;
+// }
+
+// const Layout = ({ children }: LayoutProps) => {
+//   return (
+//     <ClerkProvider>
+//     <html lang="en">
+//       <body className={`${inter.className} text-base-content`}>
+//         <div className={styles.container}>
+//           <div className={styles.menu}>
+//             <Sidebar />
+//           </div>
+//           <div className={styles.content}>
+//             <div className="sticky top-0 z-10">
+//               <Navbar />
+//             </div>
+//             <div className="h-screen overflow-y-auto bg-base-200">
+//               {children}
+//             </div>
+//             <div className="sticky overflow-hidden">
+
+//         <Footer/>
+//             </div>
+//           </div>
+//         </div>
+//       </body>
+//     </html>
+//     </ClerkProvider>
+//   );
+// };
+
+// export default Layout;
+
+
 import { ReactNode } from 'react';
 import Sidebar from '@/components/Sidebar';
 import styles from '@/app/main.module.css';
@@ -61,11 +118,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import {
   ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
+} from '@clerk/nextjs';
 import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ["latin"] });
@@ -74,6 +127,7 @@ export const metadata: Metadata = {
   title: "MyLibrary",
   description: "Easily access and download a wide range of IT eBooks. Browse by category and get the resources you need with just a click—no limits, no hassle. Dive into your next great read today!",
 };
+
 interface LayoutProps {
   children: ReactNode;
 }
@@ -81,24 +135,26 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={`${inter.className} text-base-content`}>
-        <div className={styles.container}>
-          <div className={styles.menu}>
-            <Sidebar />
-          </div>
-          <div className={styles.content}>
-            <div className="sticky top-0 z-10">
-              <Navbar />
+      <html lang="en">
+        <body className={`${inter.className} text-base-content`}>
+          <div className="flex min-h-screen">
+            <div className={styles.menu}>
+              <Sidebar />
             </div>
-            <div className="h-screen overflow-y-auto bg-base-200">
-              {children}
+            <div className="flex-1 flex flex-col">
+              <div className="sticky top-0 z-10">
+                <Navbar />
+              </div>
+              <main className="flex-1 overflow-y-auto p-4 bg-base-200">
+                {children}
+              </main>
+              <div className="sticky bottom-0">
+                <Footer />
+              </div>
             </div>
-        <Footer/>
           </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
     </ClerkProvider>
   );
 };
